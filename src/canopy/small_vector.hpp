a92f366d3c3d3699213a5f9a5cffa103243c23d6
Toby Brull <Toby.Brull@gmail.com>
2024-12-13 23:07:03 +0000

    added short_vector_t

---
+++ src/canopy/small_vector.hpp
@@ -0,0 +1,124 @@
+#pragma once
+
+#include "assert.hpp"
+#include "types.hpp"
+
+#include <memory>
+
+namespace silva {
+  //
+  // TODO: Exception safety
+  //
+  template<typename T, index_t N>
+  struct small_vector_t {
+    alignas(T) char storage[sizeof(T) * N];
+    index_t size = 0;
+
+    ~small_vector_t();
+
+    small_vector_t() = default;
+
+    small_vector_t(index_t init_size, const T& init_value = T{});
+
+    small_vector_t(small_vector_t&&);
+    small_vector_t& operator=(small_vector_t&&);
+    small_vector_t(const small_vector_t&);
+    small_vector_t& operator=(const small_vector_t&);
+
+    template<typename... Args>
+    void emplace_back(Args&&...);
+
+    T& operator[](index_t);
+    const T& operator[](index_t) const;
+  };
+}
+
+// IMPLEMENTATION
+
+namespace silva {
+  template<typename T, index_t N>
+  small_vector_t<T, N>::~small_vector_t()
+  {
+    for (index_t i = 0; i < size; ++i) {
+      std::destroy_at<T>(&(*this)[i]);
+    }
+  }
+
+  template<typename T, index_t N>
+  small_vector_t<T, N>::small_vector_t(index_t init_size, const T& init_value) : size(init_size)
+  {
+    for (index_t i = 0; i < size; ++i) {
+      std::construct_at<T>(&(*this)[i], init_value);
+    }
+  }
+
+  template<typename T, index_t N>
+  small_vector_t<T, N>::small_vector_t(small_vector_t&& other) : size(other.size)
+  {
+    for (index_t i = 0; i < size; ++i) {
+      std::construct_at<T>(&(*this)[i], std::move(other[i]));
+      std::destroy_at<T>(&other[i]);
+    }
+    other.size = 0;
+  }
+
+  template<typename T, index_t N>
+  small_vector_t<T, N>& small_vector_t<T, N>::operator=(small_vector_t&& other)
+  {
+    if (this != &other) {
+      for (index_t i = 0; i < size; ++i) {
+        std::destroy_at<T>(&(*this)[i]);
+      }
+      size = other.size;
+      for (index_t i = 0; i < size; ++i) {
+        std::construct_at<T>(&(*this)[i], std::move(other[i]));
+        std::destroy_at<T>(&other[i]);
+      }
+      other.size = 0;
+    }
+    return *this;
+  }
+
+  template<typename T, index_t N>
+  small_vector_t<T, N>::small_vector_t(const small_vector_t& other) : size(other.size)
+  {
+    for (index_t i = 0; i < size; ++i) {
+      std::construct_at<T>(&(*this)[i], other[i]);
+    }
+  }
+
+  template<typename T, index_t N>
+  small_vector_t<T, N>& small_vector_t<T, N>::operator=(const small_vector_t& other)
+  {
+    if (this != &other) {
+      for (index_t i = 0; i < size; ++i) {
+        std::destroy_at<T>(&(*this)[i]);
+      }
+      size = other.size;
+      for (index_t i = 0; i < size; ++i) {
+        std::construct_at<T>(&(*this)[i], std::move(other[i]));
+      }
+    }
+    return *this;
+  }
+
+  template<typename T, index_t N>
+  template<typename... Args>
+  void small_vector_t<T, N>::emplace_back(Args&&... args)
+  {
+    SILVA_ASSERT(size < N);
+    std::construct_at<T>(&(*this)[size], std::forward<Args>(args)...);
+    ++size;
+  }
+
+  template<typename T, index_t N>
+  T& small_vector_t<T, N>::operator[](const index_t index)
+  {
+    return *reinterpret_cast<T*>(storage + index * sizeof(T));
+  }
+  template<typename T, index_t N>
+  const T& small_vector_t<T, N>::operator[](const index_t index) const
+  {
+    return *reinterpret_cast<const T*>(storage + index * sizeof(T));
+  }
+}
