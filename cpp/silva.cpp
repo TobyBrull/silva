da54a311e90a6aa57370b2e09ab32eb18b373d90
Toby Brull <Toby.Brull@gmail.com>
2025-03-08 00:18:10 +0000

    remove file

--- cpp/silva.cpp
+++ cpp/silva.cpp
@@ -3,8 +3,4 @@
 #include "parse_root.hpp"
 
 namespace silva {
-  unique_ptr_t<parse_root_t> silva_parse_root(token_context_ptr_t tcp)
-  {
-    return SILVA_EXPECT_ASSERT(parse_root_t::create(tcp, "silva.seed", string_t{silva_seed}));
-  }
 }
