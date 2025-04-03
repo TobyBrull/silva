594ff79d513713338ec59a5b254b14eec772b2ef
Toby Brull <Toby.Brull@gmail.com>
2025-04-04 00:06:48 +0100

    simplify keeping track of token locations

--- cpp/syntax/tokenization.cpp
+++ cpp/syntax/tokenization.cpp
@@ -10,52 +10,17 @@ namespace silva {
   tokenization_t tokenization_t::copy() const
   {
     return tokenization_t{
-        .context  = context,
-        .filepath = filepath,
-        .text     = text,
-        .tokens   = tokens,
-        .lines    = lines,
+        .context         = context,
+        .filepath        = filepath,
+        .text            = text,
+        .tokens          = tokens,
+        .token_locations = token_locations,
     };
   }
 
   const token_info_t* tokenization_t::token_info_get(const index_t token_index) const
   {
     return &context->token_infos[tokens[token_index]];
-  }
-
-  const tokenization_t::line_data_t*
-  tokenization_t::binary_search_line(const index_t token_index) const
-  {
-    auto it =
-        std::ranges::lower_bound(lines, token_index, std::less<>{}, [](const line_data_t& line) {
-          return line.token_index;
-        });
-    SILVA_ASSERT(!lines.empty());
-    if (it == lines.end() || it->token_index != token_index) {
-      SILVA_ASSERT(it != lines.begin());
-      --it;
-    }
-    while (true) {
-      SILVA_ASSERT(it != lines.end());
-      const auto next_it = std::next(it);
-      if (next_it != lines.end() && next_it->token_index == token_index) {
-        it = next_it;
-      }
-      else {
-        break;
-      }
-    }
-    return &(*it);
-  }
-
-  namespace impl {
-    void start_new_line(tokenization_t* tokenization, const index_t source_code_offset)
-    {
-      tokenization->lines.push_back(tokenization_t::line_data_t{
-          .token_index        = static_cast<index_t>(tokenization->tokens.size()),
-          .source_code_offset = source_code_offset,
-      });
-    }
   }
 
   token_id_t token_context_get_token_id_from_info(token_context_t* tc,
@@ -83,57 +48,33 @@ namespace silva {
   expected_t<unique_ptr_t<tokenization_t>>
   tokenize(token_context_ptr_t tcp, filesystem_path_t filepath, string_t text_arg)
   {
-    auto retval      = std::make_unique<tokenization_t>();
-    retval->filepath = std::move(filepath);
-    retval->text     = std::move(text_arg);
-    retval->context  = tcp;
-    impl::start_new_line(retval.get(), 0);
-    index_t text_index       = 0;
+    auto retval        = std::make_unique<tokenization_t>();
+    retval->filepath   = std::move(filepath);
+    retval->text       = std::move(text_arg);
+    retval->context    = tcp;
+    index_t text_index = 0;
+    tokenization_t::location_t loc;
     const string_view_t text = retval->text;
     while (text_index < text.size()) {
       const auto [tokenized_str, token_cat] = tokenize_one(text.substr(text_index));
       text_index += tokenized_str.size();
+      const auto old_loc = loc;
+      loc.column += tokenized_str.size();
       if (token_cat != NONE) {
-        const token_id_t tii =
-            token_context_get_token_id_from_info(tcp.get(),
-                                                 token_info_t{
-                                                     .category = token_cat,
-                                                     .str      = string_t{tokenized_str},
-                                                 });
+        token_info_t ti{
+            .category = token_cat,
+            .str      = string_t{tokenized_str},
+        };
+        const token_id_t tii = token_context_get_token_id_from_info(tcp.get(), std::move(ti));
         retval->tokens.push_back(tii);
+        retval->token_locations.push_back(old_loc);
       }
       else if (tokenized_str == "\n") {
-        impl::start_new_line(retval.get(), text_index);
+        loc.line_num += 1;
+        loc.column = 0;
       }
     }
     return retval;
-  }
-
-  tuple_t<index_t, index_t> tokenization_t::compute_line_and_column(const index_t token_index) const
-  {
-    const auto* line_data          = binary_search_line(token_index);
-    const string_view_t rest       = string_view_t{text}.substr(line_data->source_code_offset);
-    const index_t line_token_index = token_index - line_data->token_index;
-    index_t seen_tokens            = 0;
-    index_t column                 = 0;
-    while (column < rest.size()) {
-      const auto [tokenized_str, token_cat] = tokenize_one(rest.substr(column));
-      if (token_cat == NONE) {
-        column += tokenized_str.size();
-      }
-      else {
-        SILVA_ASSERT(tokenized_str != "\n");
-        seen_tokens += 1;
-        if (seen_tokens > line_token_index) {
-          break;
-        }
-        else {
-          column += tokenized_str.size();
-        }
-      }
-    }
-    const index_t line = line_data - lines.data();
-    return {line, column};
   }
 
   string_t tokenization_t::to_string() const
@@ -142,7 +83,7 @@ namespace silva {
     for (index_t token_index = 0; token_index < tokens.size(); ++token_index) {
       const token_id_t tii      = tokens[token_index];
       const token_info_t* info  = &context->token_infos[tii];
-      const auto [line, column] = compute_line_and_column(token_index);
+      const auto [line, column] = token_locations[token_index];
       retval += fmt::format("[{:3}] {:3}:{:<3} {}\n", token_index, line + 1, column + 1, info->str);
     }
     return retval;
--- cpp/syntax/tokenization.hpp
+++ cpp/syntax/tokenization.hpp
@@ -6,36 +6,26 @@
 #include "token_context.hpp"
 
 namespace silva {
-  struct tokenization_t : public sprite_t {
+  struct tokenization_t : public menhir_t {
     token_context_ptr_t context;
 
     filesystem_path_t filepath;
     string_t text;
     vector_t<token_id_t> tokens;
 
-    struct line_data_t {
-      // The index in "tokens" of the first token in that line. If a line does not contain any
-      // tokens, this is the index of any token in a following line. What if a token spans multiple
-      // lines (could only happend for strings)?
-      index_t token_index = 0;
-      // Offset of the start of this line within "text".
-      index_t source_code_offset = 0;
-
-      friend auto operator<=>(const line_data_t&, const line_data_t&) = default;
+    struct location_t {
+      index_t line_num = 0;
+      index_t column   = 0;
     };
-    // Contains one entry for each line in "text".
-    vector_t<line_data_t> lines;
+    vector_t<location_t> token_locations;
 
     tokenization_t copy() const;
 
     const token_info_t* token_info_get(index_t token_index) const;
 
-    const line_data_t* binary_search_line(index_t token_index) const;
-
-    tuple_t<index_t, index_t> compute_line_and_column(index_t token_index) const;
-
     string_t to_string() const;
   };
+  using tokenization_ptr_t = ptr_t<tokenization_t>;
 
   struct token_position_t {
     const tokenization_t* tokenization{nullptr};
@@ -63,7 +53,7 @@ namespace silva {
         [](const byte_t* ptr, const index_t size) -> string_or_view_t {
           SILVA_ASSERT(size == sizeof(token_position_t));
           const token_position_t tp = bit_cast_ptr<token_position_t>(ptr);
-          const auto [line, column] = tp.tokenization->compute_line_and_column(tp.token_index);
+          const auto [line, column] = tp.tokenization->token_locations[tp.token_index];
           const string_t filename   = tp.tokenization->filepath.filename().string();
           return string_or_view_t{fmt::format("[{}:{}:{}]", filename, line + 1, column + 1)};
         });
