23c4075f83da171280aba5c4c4b8f5184ec6186b
Toby Brull <Toby.Brull@gmail.com>
2025-06-27 00:35:14 +0100

    libfmt via FetchContent

---
+++ cmake/fmtlib.cmake
@@ -0,0 +1,9 @@
+include(FetchContent)
+FetchContent_Declare(
+  fmtlib
+  GIT_REPOSITORY  https://github.com/fmtlib/fmt.git
+  GIT_TAG         11.2.0
+  GIT_SHALLOW     TRUE
+  GIT_PROGRESS    TRUE
+)
+FetchContent_MakeAvailable(fmtlib)
