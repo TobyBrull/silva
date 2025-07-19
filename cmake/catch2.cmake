c75ed495d9e0a76cd55414c61c69d1831160b811
Toby Brull <Toby.Brull@gmail.com>
2025-07-19 11:12:56 +0100

    catch2 via FetchContent

---
+++ cmake/catch2.cmake
@@ -0,0 +1,9 @@
+include(FetchContent)
+FetchContent_Declare(
+  Catch2
+  GIT_REPOSITORY  https://github.com/catchorg/Catch2.git
+  GIT_TAG         v3.8.1
+  GIT_SHALLOW     TRUE
+  GIT_PROGRESS    TRUE
+)
+FetchContent_MakeAvailable(Catch2)
