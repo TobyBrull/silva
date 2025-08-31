010ffca9cd93bb5a896fce5879647d00f5240a8e
Toby Brull <Toby.Brull@gmail.com>
2025-08-31 23:12:26 +0100

    make demo.sh faster

--- demo.sh
+++ demo.sh
@@ -10,8 +10,6 @@ trap 'rm -f "$TEMPFILE"' EXIT
 ./build/cpp/silva_fern silva/syntax/01-simple.fern
 ./build/cpp/silva_fern silva/syntax/01-broken.fern 2>"$TEMPFILE" || true
 cat "$TEMPFILE"
-time ./build/cpp/silva_fern silva/syntax/01-large.fern --process=none --seed-engine=false
-time ./build/cpp/silva_fern silva/syntax/01-large.fern --process=none --seed-engine=true
 
 ./build/cpp/silva_syntax silva/syntax/01-simplest.fern
 SEED_EXEC_TRACE=true ./build/cpp/silva_syntax silva/syntax/01-simplest.fern --action=none
@@ -21,7 +19,5 @@ SEED_EXEC_TRACE=true ./build/cpp/silva_syntax silva/syntax/01-simplest.fern --ac
 
 ./build/cpp/silva_syntax silva/soil/soil.silva silva/soil/example.silva
 
-./build/cpp/silva_lox cpp/zoo/lox/example.lox --use-interpreter=true
-./build/cpp/silva_lox cpp/zoo/lox/example.lox --use-interpreter=false
 ./build/cpp/silva_lox cpp/zoo/lox/lox.lox --use-interpreter=true < cpp/zoo/lox/example.lox
 ./build/cpp/silva_lox cpp/zoo/lox/lox.lox --use-interpreter=false < cpp/zoo/lox/example.lox
