f620e346cbdc2b259690d7ce2a84fc8649e38209
Toby Brull <Toby.Brull@gmail.com>
2025-11-29 23:37:11 +0000

    some notes in relation to unicode

---
+++ silva/playground/unicode.md
@@ -0,0 +1,13 @@
+Unicode Notes
+=============
+
+Parentheses
+-----------
+
+Possible letters for parentheses:
+« »
+‹ ›
+≪ ≫
+❬ ❭
+
+https://stackoverflow.com/questions/13535172/list-of-all-unicodes-open-close-brackets
---
+++ silva/playground/v1.seed.cpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "syntax/parse_tree.hpp"
+
+// Recommended reading:
+//
+//  * The quite readable original paper by Bryan Ford "Parsing Expression Grammars: A
+//    Recognition-Based Syntactic Foundation"
+//    https://bford.info/pub/lang/peg.pdf
+//
+//  * Guido van Rossum's "PEG Parsing Series"
+//    https://medium.com/@gvanrossum_83706/peg-parsing-series-de5d41b2ed60
+
+namespace silva::seed {
+
+  // A program in the Seed language describes a way to turn a tokenization_t
+  // into a parse_tree_t. The actual algorithm to do this is implemented
+  // in "seed::interpreter_t".
+
+  const string_view_t seed_str = R"'(
+    - Seed = [
+      - ⊙ = ( '-' Rule ) *
+      - Rule = Nonterminal ( '=' '[' ⊙ ']' | ExprOrAlias | '=/' Axe )
+      - ExprOrAlias = ( '=' | '=>' ) Expr
+      - Expr =/ Atom [
+        - Parens    = nest  atom_nest '(' ')'
+        - Prefix    = rtl   prefix 'not'
+        - Postfix   = ltr   postfix '?' '*' '+'
+        - Concat    = ltr   infix_flat concat
+        - And       = ltr   infix_flat 'but_then'
+        - Or        = ltr   infix_flat '|'
+      ]
+      - Atom => NonterminalMaybeVar | Terminal | Function
+      - NonterminalMaybeVar = Nonterminal ( '->' Variable ) ?
+      - Variable = identifier / '^[a-z].*_v$'
+      - Function = [
+         - ⊙ = Name '(' Args ')'
+         - Name = identifier / '^[a-z].*_f$'
+         - Args = Arg ( ',' Arg ) *
+         - Arg => ↖.Expr | ↖.Variable
+      ]
+      - Nonterminal = [
+        - ⊙ = Base ( '.' Base ) *
+        - Base = 'ρ' | '⊙' | '↖' | identifier / '^[A-Z]'
+      ]
+      - Terminal = string
+                 | 'identifier' ( '/' string ) ?
+                 | 'operator' ( '/' string ) ?
+                 | 'keywords_of' Nonterminal
+                 | 'string' | 'number'
+                 | 'any' | 'ε' | 'end_of_file'
+    ]
+    - None = ε
+  )'";
+
+  // Invariant (pseudo-code):
+  //    standard_seed_engine()->apply(tokenization, "Seed") == seed_parse(tokenization)
+  expected_t<parse_tree_ptr_t> parse(tokenization_ptr_t);
+}
