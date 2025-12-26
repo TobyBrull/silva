4bbc2f4d94f20c2edea51a66ee220f398c521e8b
Toby Brull <Toby.Brull@gmail.com>
2025-12-26 00:23:41 +0000

    cmake-format

--- cmake/catch2.cmake
+++ cmake/catch2.cmake
@@ -1,10 +1,11 @@
 include(FetchContent)
-set(CATCH_CONFIG_CONSOLE_WIDTH 256 CACHE STRING "wide console")
+set(CATCH_CONFIG_CONSOLE_WIDTH
+    256
+    CACHE STRING "wide console")
 FetchContent_Declare(
   Catch2
-  GIT_REPOSITORY  https://github.com/catchorg/Catch2.git
-  GIT_TAG         v3.8.1
-  GIT_SHALLOW     TRUE
-  GIT_PROGRESS    TRUE
-)
+  GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+  GIT_TAG v3.8.1
+  GIT_SHALLOW TRUE
+  GIT_PROGRESS TRUE)
 FetchContent_MakeAvailable(Catch2)
--- cmake/fmtlib.cmake
+++ cmake/fmtlib.cmake
@@ -1,9 +1,8 @@
 include(FetchContent)
 FetchContent_Declare(
   fmtlib
-  GIT_REPOSITORY  https://github.com/fmtlib/fmt.git
-  GIT_TAG         11.2.0
-  GIT_SHALLOW     TRUE
-  GIT_PROGRESS    TRUE
-)
+  GIT_REPOSITORY https://github.com/fmtlib/fmt.git
+  GIT_TAG 11.2.0
+  GIT_SHALLOW TRUE
+  GIT_PROGRESS TRUE)
 FetchContent_MakeAvailable(fmtlib)
