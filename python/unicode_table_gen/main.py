191675ee85fb5d5d0b138e3b975890c907adf342
Toby Brull <Toby.Brull@gmail.com>
2026-01-01 20:08:35 +0000

    unicode-table-gen: parentheses

--- python/unicode_table_gen/main.py
+++ python/unicode_table_gen/main.py
@@ -6,17 +6,15 @@ import requests
 import argparse
 import collections
 import dataclasses
-import enum
-
-import numpy as np
-
-from typing import Any
+import pprint
 
 unicode_files = (
     "DerivedNormalizationProps.txt",
     "DerivedCoreProperties.txt",
     "UnicodeData.txt",
 )
+
+Codepoint = int
 
 
 def handle_download(args):
@@ -34,6 +32,15 @@ def handle_download(args):
         print(f"Downloaded: {target_filename}")
 
 
+def codepoint_to_str(cp: Codepoint) -> str:
+    retval = ''
+    try:
+        retval = chr(cp)
+    except:
+        pass
+    return retval
+
+
 @dataclasses.dataclass
 class UnicodeProperty:
     name: str = ""
@@ -43,46 +50,39 @@ class UnicodeProperty:
         if ".." in codepoint_str:
             codepoint_range = codepoint_str.split('..', 1)
             assert len(codepoint_range) == 2
-            codepoint_from = int(codepoint_range[0], 16)
-            codepoint_upto = int(codepoint_range[1], 16)
-            for codepoint in range(codepoint_from, codepoint_upto + 1):
-                self.recognise_impl(codepoint, value)
+            codepoint_from = Codepoint(codepoint_range[0], 16)
+            codepoint_upto = Codepoint(codepoint_range[1], 16)
+            for cp in range(codepoint_from, codepoint_upto + 1):
+                self.recognise_impl(cp, value)
         else:
-            codepoint = int(codepoint_str, 16)
-            self.recognise_impl(codepoint, value)
+            cp = Codepoint(codepoint_str, 16)
+            self.recognise_impl(cp, value)
 
     def recognise_one(self, codepoint_str: str, value: str):
-        codepoint = int(codepoint_str, 16)
-        self.recognise_impl(codepoint, value)
+        cp = Codepoint(codepoint_str, 16)
+        self.recognise_impl(cp, value)
 
-    def recognise_impl(self, codepoint: int, value: str):
-        self.values[codepoint] = value
+    def recognise_impl(self, cp: Codepoint, value: str):
+        self.values[cp] = value
 
     def to_string(self) -> str:
         retval = f"========= {self.name} =========\n"
-        mm = collections.defaultdict(list)
-        for codepoint, val in enumerate(self.values):
-            utf8 = ''
-            try:
-                utf8 = chr(codepoint)
-            except:
-                pass
-            mm[val].append(utf8)
-        for k, v in sorted(mm.items()):
-            retval += f'{k:20} [{len(v):7}] {v[:10]}\n'
+        mm: dict[str, list[Codepoint]] = collections.defaultdict(list)
+        for cp, val in enumerate(self.values):
+            mm[val].append(cp)
+        for k, codepoints in sorted(mm.items()):
+            retval += f"{k:20} [{len(codepoints):7}] "
+            for cp in codepoints[:10]:
+                retval += f"0x{cp:04x}, "
+            retval += "\n"
         retval += "\n"
         return retval
 
-    def get_by_value(self, value: str) -> list[str]:
+    def get_by_value(self, value: str) -> list[Codepoint]:
         retval = []
-        for codepoint, val in enumerate(self.values):
+        for cp, val in enumerate(self.values):
             if val == value:
-                utf8 = ''
-                try:
-                    utf8 = chr(codepoint)
-                except:
-                    pass
-                retval.append(utf8)
+                retval.append(cp)
         return retval
 
     def get_mask(self, value: str) -> list[bool]:
@@ -127,7 +127,7 @@ def parse_derived_file(filename: str) -> list[UnicodeProperty]:
 @dataclasses.dataclass
 class RangeStart:
     name: str
-    codepoint: str
+    codepoint_str: str
     value: str
 
 
@@ -143,13 +143,13 @@ def parse_unicode_data(filename: str) -> tuple[list[str], list[UnicodeProperty]]
                 row_name = row_name[1:-1]
                 if row_name.endswith("First"):
                     assert range_start is None
-                    range_start = RangeStart(name=row_name[:-5], codepoint=row[0], value=row[2])
+                    range_start = RangeStart(name=row_name[:-5], codepoint_str=row[0], value=row[2])
                 elif row_name.endswith("Last"):
                     assert range_start is not None
                     assert range_start.name + "Last" == row_name
                     assert range_start.value == row[2]
-                    prop.recognise(f"{range_start.codepoint}..{row[0]}", row[2])
-                    prop.recognise(f"{range_start.codepoint}..{row[0]}", row[1])
+                    prop.recognise(f"{range_start.codepoint_str}..{row[0]}", row[2])
+                    prop.recognise(f"{range_start.codepoint_str}..{row[0]}", row[1])
                     range_start = None
                 else:
                     raise Exception(
@@ -172,10 +172,10 @@ def make_mapping(map: dict[str, str], default: str = "Invalid"):
 def apply_mapping(prop: UnicodeProperty, mapping, new_name: str | None = None) -> UnicodeProperty:
     retval = UnicodeProperty()
     retval.name = new_name or prop.name
-    for codepoint in range(len(prop.values)):
-        value = prop.values[codepoint]
+    for cp in range(len(prop.values)):
+        value = prop.values[cp]
         new_value = mapping(value)
-        retval.recognise_impl(codepoint, new_value)
+        retval.recognise_impl(cp, new_value)
     return retval
 
 
@@ -223,13 +223,13 @@ def process_props(loaded_props: dict[str, UnicodeProperty]) -> list[UnicodePrope
             },
             "Other",
         ),
-        new_name="Parentheses",
+        new_name="GeneralCategory",
     )
-    retval.append(general_cat)
+    # retval.append(general_cat)
 
-    retval.append(apply_mapping(loaded_props["Derived_XID_Start"], YES_mapping))
-    retval.append(apply_mapping(loaded_props["Derived_XID_Continue"], YES_mapping))
-    retval.append(apply_mapping(loaded_props["Derived_Math"], YES_mapping))
+    retval.append(apply_mapping(loaded_props["Derived_XID_Start"], YES_mapping, new_name="DerivedXIDStart"))
+    retval.append(apply_mapping(loaded_props["Derived_XID_Continue"], YES_mapping, new_name="DerivedXIDContinue"))
+    retval.append(apply_mapping(loaded_props["Derived_Math"], YES_mapping, new_name="DerivedMath"))
 
     nfc_qc = apply_mapping(
         loaded_props["Derived_NFC_QC"],
@@ -240,6 +240,7 @@ def process_props(loaded_props: dict[str, UnicodeProperty]) -> list[UnicodePrope
             },
             "Yes",
         ),
+        new_name="DerivedNFCQuickCheck"
     )
     nfc_qc.update_masked(general_cat.get_mask("Unassigned"), "No")
     retval.append(nfc_qc)
@@ -306,6 +307,55 @@ def make_multi_stage_table(num_lower_bits: int, props: list[UnicodeProperty]) ->
     return MultistageTable(num_lower_bits, stage_1, stage_2, stage_3)
 
 
+@dataclasses.dataclass
+class Parentheses:
+    name: str = ""
+    left: Codepoint = -1
+    right: Codepoint = -1
+
+    def is_complete(self):
+        return self.left != -1 and self.right != -1
+
+    def __repr__(self):
+        retval = ""
+        retval += f"0x{self.left:04x} 0x{self.right:04x} "
+        retval += f"{codepoint_to_str(self.left)}...{codepoint_to_str(self.right)}"
+        retval += f" : {self.name}"
+        return retval
+
+
+def discover_parentheses(
+    general_category: UnicodeProperty, codepoint_names: list[str]
+) -> tuple[list[Parentheses], UnicodeProperty]:
+    left_right: dict[str, Parentheses] = collections.defaultdict(Parentheses)
+    for cp, name in enumerate(codepoint_names):
+        if "LEFT" in name and "RIGHT" in name:
+            continue
+        if "LEFT" in name:
+            name = name.replace("LEFT", "LEFT/RIGHT")
+            left_right[name].left = cp
+        elif "RIGHT" in name:
+            name = name.replace("RIGHT", "LEFT/RIGHT")
+            left_right[name].right = cp
+
+    def is_punct(cp: Codepoint) -> bool:
+        gencat = general_category.values[cp]
+        return gencat == "Ps" or gencat == "Pe" or gencat == "Pi" or gencat == "Pf"
+
+    parens: list[Parentheses] = []
+    for name, pp in left_right.items():
+        if pp.is_complete() and is_punct(pp.left) and is_punct(pp.right):
+            parens.append(Parentheses(name=name, left=pp.left, right=pp.right))
+    parens = sorted(parens, key=lambda pp: pp.left)
+
+    parens_prop = UnicodeProperty(name="IsParenthesis", values=["No"] * 0x110000)
+    for pp in parens:
+        parens_prop.recognise_impl(pp.left, "Left")
+        parens_prop.recognise_impl(pp.right, "Right")
+
+    return parens, parens_prop
+
+
 def handle_generate(args):
     props_1 = parse_derived_file(os.path.join(args.workdir, unicode_files[0]))
     props_2 = parse_derived_file(os.path.join(args.workdir, unicode_files[1]))
@@ -315,10 +365,15 @@ def handle_generate(args):
 
     cleaned_props = process_props(loaded_props)
 
+    general_category = loaded_props["General_Category"]
+    parens, parens_prop = discover_parentheses(general_category, codepoint_names)
+
+    cleaned_props.append(parens_prop)
+
     for prop in cleaned_props:
         print(prop.to_string())
-    print(cleaned_props[0].get_by_value("PunctuationOpen"))
-    print(cleaned_props[0].get_by_value("PunctuationClose"))
+
+    pprint.pprint(parens[:10])
 
     mst = make_multi_stage_table(args.num_lower_bits, cleaned_props)
     print(mst.stats())
