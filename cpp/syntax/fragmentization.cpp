f1502b19f08ff6071642b983229673cc5dbaa19f
Toby Brull <Toby.Brull@gmail.com>
2026-01-04 23:38:29 +0000

    handle newlines in fragmentization

--- cpp/syntax/fragmentization.cpp
+++ cpp/syntax/fragmentization.cpp
@@ -21,16 +21,43 @@ namespace silva {
     auto retval      = std::make_unique<fragmentization_t>();
     retval->filepath = std::move(descriptive_path);
 
-    auto range     = unicode::utf8_decode_generator(source_code);
-    auto it        = range.begin();
-    const auto end = range.end();
-    while (it != end) {
-      const unicode::codepoint_data_t cd = SILVA_EXPECT_FWD(*it);
-      const codepoint_category_t cc      = codepoint_category_table[cd.codepoint];
-      SILVA_EXPECT(cc != Forbidden, MINOR, "Forbidden codepoint 0x{:04x}", cd.codepoint);
-      ++it;
+    struct categorized_codepoint_data_t : public unicode::codepoint_data_t {
+      codepoint_category_t category = codepoint_category_t::Forbidden;
+      file_location_t location;
+    };
+    file_location_t loc;
+    array_t<categorized_codepoint_data_t> ccd;
+    for (auto maybe_ud: unicode::utf8_decode_generator(source_code)) {
+      const auto ud                 = SILVA_EXPECT_FWD(std::move(maybe_ud));
+      const codepoint_category_t cc = codepoint_category_table[ud.codepoint];
+      SILVA_EXPECT(cc != Forbidden, MINOR, "Forbidden codepoint 0x{:04x}", ud.codepoint);
+      ccd.emplace_back(ud, cc, loc);
+      if (ud.codepoint == U'\n') {
+        loc.line_num += 1;
+        loc.column = 0;
+      }
+      else {
+        loc.column += 1;
+      }
+      loc.byte_offset += ud.len;
     }
 
+    index_t ccd_idx = 0;
+    const index_t n = ccd.size();
+    while (ccd_idx < n) {
+      if (ccd[ccd_idx].category == Newline) {
+        retval->categories.push_back(NEWLINE);
+        retval->locations.push_back(ccd[ccd_idx].location);
+        do {
+          ++ccd_idx;
+        } while (ccd_idx < n && ccd[ccd_idx].category == Newline);
+      }
+      else {
+        ++ccd_idx;
+      }
+    }
+
+    retval->source_code = std::move(source_code);
     return {std::move(retval)};
   }
 
@@ -47,7 +74,7 @@ namespace silva {
       const fragment_category_t fc = self.categories[idx];
       stream->format("[{:3}] [", start_index);
       silva::pretty_write(fc, stream);
-      stream->format("] [{}]\n", sv);
+      stream->format("] [{}]\n", hexdump(sv));
     }
   }
 }
--- cpp/syntax/fragmentization.tpp
+++ cpp/syntax/fragmentization.tpp
@@ -3,13 +3,15 @@
 #include <catch2/catch_all.hpp>
 
 namespace silva::test {
+  using enum codepoint_category_t;
+  using enum fragment_category_t;
+
   TEST_CASE("fragmentization-data", "[fragmentization_t]")
   {
     const unicode::table_t<codepoint_category_t>& cct = silva::codepoint_category_table;
     SILVA_EXPECT_REQUIRE(cct.validate());
     REQUIRE(cct.key_size() == 0x110000);
 
-    using enum codepoint_category_t;
     CHECK(cct[U'\t'] == Forbidden);
     CHECK(cct[U'\n'] == Newline);
     CHECK(cct[U' '] == Space);
@@ -22,5 +24,12 @@ namespace silva::test {
     CHECK(cct[U'_'] == XID_Start);
   }
 
-  TEST_CASE("fragmentization", "[fragmentization_t]") {}
+  TEST_CASE("fragmentization", "[fragmentization_t]")
+  {
+    const auto frag = SILVA_EXPECT_REQUIRE(fragmentize("..", "\n\nx\n\n"));
+    const array_t<fragment_category_t> expected_categories{NEWLINE, NEWLINE};
+    CHECK(frag->categories == expected_categories);
+    const array_t<file_location_t> expected_locations{{0, 0, 0}, {2, 1, 3}};
+    CHECK(frag->locations == expected_locations);
+  }
 }
