a4f99693aefe466b66fb9b589f31fa0bf4a77674
Toby Brull <Toby.Brull@gmail.com>
2026-04-25 21:04:03 +0100

    more work on cedar grammar

--- cpp/zoo/cedar/cedar.hpp
+++ cpp/zoo/cedar/cedar.hpp
@@ -14,65 +14,64 @@ tokenizer Cedar:
   include tokenizer FreeForm
 
 language Cedar:
-  ⊙ = ( FunctionDefinition | Decl | StaticAssertDeclaration ) *
+  ⊙ = Decl *
+  Decl:
+    ⊙ = Decl.Specifiers Type.Specifier InitDeclarator ( ';' | Stmt.Compound ) \
+      | StaticAssert
+    Specifiers = ( StorageClassSpecifier | FunctionSpecifier | AlignmentSpecifier | Type.Qualifier ) *
+    StorageClassSpecifier = ( 'typedef' | 'extern' | 'static' | '_Thread_local' | 'auto' | 'register' )
+    FunctionSpecifier = 'inline' | '_Noreturn'
+    AlignmentSpecifier = ( '_Alignas' | 'alignas' ) '(' ( TypeName | Expr.Const ) ')'
+    StaticAssert = '_Static_assert' '(' Expr.Const ',' string ')' ';'
 
-  FunctionDefinition = DeclarationSpecifiers Declarator Decl * Stmt.Compound
-  Decl = DeclarationSpecifiers InitDeclaratorList ? ';' | StaticAssertDeclaration
-  StaticAssertDeclaration = '_Static_assert' '(' Expr.Const ',' string ')' ';'
+  Type:
+    Specifier = ( 'void' | 'char' | 'short' | 'int' | 'long'
+                | 'float' | 'double' | 'signed' | 'unsigned'
+                | '_Bool' | '_Complex' | '_Imaginary'
+                | AtomicTypeSpecifier
+                | StructOrUnionSpecifier
+                | EnumSpecifier
+                | TypedefName
+                )
+    Qualifier = ( 'const' | 'volatile' | 'restrict' | '_Atomic' )
 
-  DeclarationSpecifiers = ( StorageClassSpecifier | TypeSpecifier | TypeQualifier | FunctionSpecifier | AlignmentSpecifier ) *
-  AlignmentSpecifier = ( '_Alignas' | 'alignas' ) '(' ( TypeName | Expr.Const ) ')'
-  FunctionSpecifier = 'inline' | '_Noreturn'
-  StorageClassSpecifier = ( 'typedef' | 'extern' | 'static' | '_Thread_local' | 'auto' | 'register' )
-
-  TypeSpecifier = ( 'void' | 'char' | 'short' | 'int' | 'long'
-                  | 'float' | 'double' | 'signed' | 'unsigned'
-                  | '_Bool' | '_Complex' | '_Imaginary'
-                  | AtomicTypeSpecifier
-                  | StructOrUnionSpecifier
-                  | EnumSpecifier
-                  | TypedefName
-                  )
   EnumSpecifier = 'enum' identifier ? '{' Enumerator ( ',' Enumerator ) * ',' ? '}'
   Enumerator = EnumerationConstant ( '=' Expr.Const ) ?
-  TypeName = SpecifierQualifierList AbstractDeclarator
+  TypeName = SpecifierQualifierList AbstractDeclarator ?
 
   TypedefName = identifier # TODO: should only match identifiers that have previously been typedef'ed.
   EnumerationConstant = identifier
 
   AtomicTypeSpecifier = '_Atomic' '(' TypeName ')'
   StructOrUnionSpecifier = ( 'struct' | 'union' ) identifier ? ( '{' StructDeclaration * '}' ) ?
-  StructDeclaration = SpecifierQualifierList StructDeclaratorList ? | StaticAssertDeclaration
-  SpecifierQualifierList = ( TypeSpecifier | TypeQualifier ) *
+  StructDeclaration = SpecifierQualifierList StructDeclaratorList ? | Decl.StaticAssert
+  SpecifierQualifierList = ( Type.Specifier | Type.Qualifier ) *
   StructDeclaratorList = StructDeclarator ( ',' StructDeclarator ) *
   StructDeclarator = Declarator ( ':' Expr.Const ) ? | ':' Expr.Const
-  InitDeclaratorList = InitDeclarator ( ',' InitDeclarator ) *
   InitDeclarator = Declarator ( '=' Initializer ) ?
   Declarator = Pointer ? DirectDeclarator
   AbstractDeclarator = Pointer DirectAbstractDeclarator ? | DirectAbstractDeclarator
   DirectAbstractDeclarator = ( '(' AbstractDeclarator ')' | DirectAbstractDeclaratorExt ) DirectAbstractDeclaratorExt *
   DirectAbstractDeclaratorExt = CommonDeclaratorExt
-  Pointer = '*' TypeQualifier * Pointer ?
-  TypeQualifier = ( 'const' | 'volatile' | 'restrict' | '_Atomic' )
+  Pointer = '*' Type.Qualifier * Pointer ?
   DirectDeclarator = ( identifier | '(' Declarator ')' ) DirectDeclaratorExt *
   DirectDeclaratorExt = ( CommonDeclaratorExt
                         | '(' IdentifierList ')'
-                        | '[' TypeQualifier * '*' ']'
+                        | '[' Type.Qualifier * '*' ']'
                         )
   CommonDeclaratorExt = alias ( '[' ']'
                               | '[' '*' ']'
-                              | '[' 'static' TypeQualifier * Expr ']'
+                              | '[' 'static' Type.Qualifier * Expr ']'
                               | '[' 'static' Expr ']'
-                              | '[' TypeQualifier * 'static' Expr ']'
-                              | '[' TypeQualifier * Expr ']'
-                              | '[' TypeQualifier * ']'
+                              | '[' Type.Qualifier * 'static' Expr ']'
+                              | '[' Type.Qualifier * Expr ']'
+                              | '[' Type.Qualifier * ']'
                               | '[' Expr ']'
-                              | '(' ParameterTypeList ')'
+                              | '(' ParameterList ')'
                               | '(' ')'
                               )
-  ParameterTypeList = ParameterList ( ',' '...' ) ?
   ParameterList = ParameterDeclaration ( ',' ParameterDeclaration ) *
-  ParameterDeclaration = DeclarationSpecifiers ( Declarator | AbstractDeclarator ) ?
+  ParameterDeclaration = Decl.Specifiers Type.Specifier TypeName ( Declarator | AbstractDeclarator ) ? | '...'
   IdentifierList = identifier ( ',' identifier ) *
 
   Initializer = '{' InitializerList ',' ? '}' | Expr
