eb0a630203bba7ff8be27e62e384a541e899b462
Toby Brull <Toby.Brull@gmail.com>
2026-08-30 21:33:54 +0100

    final cleanup

--- cpp/syntax/seed_axe.hpp
+++ cpp/syntax/seed_axe.hpp
@@ -48,8 +48,9 @@ Seed.Axe:
     name_id_t name;
     name_id_ref_t atom_rule;
 
-    // Array of all literals used in this axe's definition, sorted by length. The sorting is
-    // necessary so that, e.g., '!=' is tried before '!'.
+    // Array of all literals used in this axe's definition, sorted by literal length first and then
+    // appearance in the axe's definition. The sorting is necessary so that, e.g., '!=' is tried
+    // before '!' even if '!' has higher precedence.
     array_t<fragmented_token_t> op_literals;
 
     hash_map_t<token_id_t, impl::axe_result_t> results;
